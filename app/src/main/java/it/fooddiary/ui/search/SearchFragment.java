3ab071fe600eed0e595645a9bbfd6169b6e6e0a4
matteo provezza <matteoprovezza98@gmail.com>
2021-04-17 11:36:55 +0200

    sistemata search bar e aggiunta addCaloriesAlert

--- app/src/main/java/it/fooddiary/ui/search/SearchFragment.java
+++ app/src/main/java/it/fooddiary/ui/search/SearchFragment.java
@@ -2,15 +2,18 @@ package it.fooddiary.ui.search;
 
 
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.NumberPicker;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 
@@ -22,6 +25,7 @@ import it.fooddiary.R;
 public class SearchFragment extends Fragment {
 
     private TabLayout TabItem;
+    private MaterialAlertDialogBuilder addCaloriesDialog;
 
     @Nullable
     @Override
@@ -31,8 +35,32 @@ public class SearchFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_search, container, false);
         MainActivity main = (MainActivity) getActivity();
         main.setSearchToolbar();
+        addCaloriesDialog = new MaterialAlertDialogBuilder(main);
+        NumberPicker numberPicker;
+        numberPicker = new NumberPicker(main);
+        numberPicker.setMinValue(1);
+        numberPicker.setMaxValue(10000);
+        addCaloriesDialog.setView(numberPicker);
+        addCaloriesDialog.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+        addCaloriesDialog.setNegativeButton(R.string.cancel,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // TODO Auto-generated method stub
+                        return;
+                    }
+                });
+
 
         return root;
+    }
+    public void onAddCaloriesClick(View root){
+
     }
 
 
