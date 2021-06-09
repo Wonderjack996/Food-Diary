9edbd6ed14e2709987099901247966c1ad750e22
Giacomo Savazzi <giacomosavazzi996@gmail.com>
2021-06-09 09:17:07 +0200

    Sistemati colori number picker e editText

--- app/src/main/java/it/fooddiary/utils/Constants.java
+++ app/src/main/java/it/fooddiary/utils/Constants.java
@@ -12,7 +12,7 @@ public class Constants {
                     //"(?=.*[a-z])" +         //at least 1 lower case letter
                     //"(?=.*[A-Z])" +         //at least 1 upper case letter
                     "(?=.*[a-zA-Z])" +        //any letter
-                    "(?=.*[@#$%^&+=!])" +      //at least 1 special character
+                    "(?=.*[@#$%^&+=!?_-])" +  //at least 1 special character
                     "(?=\\S+$)" +             //no white spaces
                     ".{8,}" +                 //at least 8 characters
                     "$");
